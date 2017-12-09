8a525c9ad9c970ceaec363bab1eabe6432e59d29
nickdg <delgrosso@bio.lmu.de>
2017-12-09 16:57:45 +0100

    updates after rehearsal.

--- my_day.py
+++ my_day.py
@@ -13,66 +13,75 @@ logging.getLogger('flask_ask').setLevel(logging.INFO)
 @ask.launch
 def launch():
     card_title = 'Message in a Bottle'
-    text = 'Hi Anna! What would you like to do?'
-    prompt = 'You can ask to record a message, hear a random message, or check your inbox.'
-    return question(text).reprompt(prompt).simple_card(card_title, text)
+    text = 'Hi Anna! What would you like to do? You can ask to record a message, hear a message from the community, or check your inbox.'
+    return question(text)
 
-
+#
 @app.route('/hear/random')
 def view_method():
-     path_to_file = "recordings/nicktest2.mp3"
+     path_to_file = "recordings/samantha_2.mp3"
      return send_file(path_to_file, mimetype="audio/mp3", as_attachment=True, attachment_filename='random.mp3')
 
 
 @ask.intent('BrowseRandomIntent')
 def listen_to_random():
-    speech = "Nick from Munich says,"
+    speech = "Great!  I’ve discovered Samantha.  She also lives in Munich!  Playing her message now."
     stream_url = 'https://d6143fb0.ngrok.io/hear/random'
     return audio(speech).play(stream_url)
 
-
+#
 # 'ask audio_skil Play the sax
 @ask.intent('CheckMessagesIntent')
 def check_messages():
-    speech = 'You have two messages, one from Samantha and one from Nick.'
-    return statement(speech)
+    speech = 'You have a message from Nick.'
+    prompt = "I didn't catch that."
+    return question(speech).reprompt(prompt)
+
+
+@app.route('/hear/nick')
+def play_nick():
+     path_to_file = "recordings/nick_3.mp3"
+     return send_file(path_to_file, mimetype="audio/mp3", as_attachment=True, attachment_filename='random.mp3')
+
+
+@ask.intent('PlayMessageIntent')
+def read_nick_message():
+    speech = "Here is Nick's Message:"
+    stream_url = 'https://d6143fb0.ngrok.io/hear/nick'
+    return audio(speech).play(stream_url)
 
 
 @ask.intent('RecordMessageIntent')
 def record_message():
-    speech = '<speak>Great!  Starting recording.<break time="3s"/> </speak>'
+    speech = """<speak>
+    Great!  Starting recording.<break time="9s"/><break time="9s"/><break time="1s"/>
+    </speak>"""
     return question(speech)
 
-
+#
 @ask.intent('EndMessageIntent')
 def end_recording():
-    speech = 'Your message is recorded. Would you like me to send it to the community?'
+    speech = 'Your message is recorded. Would you like me to send it?'
     prompt = "I didn't catch that.  Should I send your message?"
     return question(speech).reprompt(prompt)
-
+#
+#
 @ask.intent('AMAZON.YesIntent')
-def end_recording():
-    speech = 'Recording sent.'
-    return statement(speech)
-
-
+def complete_recording():
+    speech = 'Done! You have new messages.  You can hear from your existing friends or explore new connections.'
+    return question(speech)
+#
+#
 @ask.intent('AMAZON.NoIntent')
-def end_recording():
+def no_recording():
     speech = 'Message not sent.  Recording deleted.'
     return statement(speech)
-
-# @ask.intent('ListenMessageIntent')
-# def listen_to_(user):
-#     speech = "{{user}} from Munich says,"
-#     stream_url = 'https://d6143fb0.ngrok.io/hear/random'
-#     return audio(speech).play(stream_url, offset=93000)
 #
 #
-# @ask.intent('ReplyIntent')
-# def reply_to_message():
-#     speech = 'yeah you got it! This is an audio stream from a web server.'
-#     stream_url = 'https://ia800203.us.archive.org/27/items/CarelessWhisper_435/CarelessWhisper.ogg'
-#     return audio(speech).play(stream_url)
+@ask.intent('SendContactIntent')
+def send_contact():
+    speech = "I’ve sent Nick your contact information.  Congratulations on making a new friend!"
+    return statement(speech)
 
 
 @ask.intent('AMAZON.PauseIntent')
